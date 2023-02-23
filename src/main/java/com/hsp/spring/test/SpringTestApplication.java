b9d76c114af946758cb102d944fe22e91e7346c1
DESKTOP-I633S8U\user <93star2@gmail.com>
2023-02-23 16:48:45 +0900

    [박현석] -database연동 configuration

--- src/main/java/com/hsp/spring/test/SpringTestApplication.java
+++ src/main/java/com/hsp/spring/test/SpringTestApplication.java
@@ -1,33 +1,17 @@
 package com.hsp.spring.test;
-
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
 
 @SpringBootApplication
-@MapperScan(basePackages="com.hsp.spring.test.*")
-public class SpringTestApplication {
 
+public class SpringTestApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestApplication.class, args);
 	}
 	
-	@Bean
-	public SqlSessionFactory sqlSessionFactory(DataSource datasource) throws Exception {
-		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-		sessionFactory.setDataSource(datasource);
-		
-		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
-		sessionFactory.setMapperLocations(res);
-		
-		return sessionFactory.getObject();
-	}
-
+	
 }
+
+
