8aa23fb425662abb88c501a2ffb2cc2068149e49
hsp9781 <>
2023-03-02 21:38:00 +0900

    [박현석] -jstl core 라이브러리2

--- src/main/java/com/hsp/spring/test/jstl/JSTLController.java
+++ src/main/java/com/hsp/spring/test/jstl/JSTLController.java
@@ -1,6 +1,10 @@
 package com.hsp.spring.test.jstl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +15,21 @@ public class JSTLController {
 	@GetMapping("/test01")
 	public String test01() {
 		return "jstl/test01";
+	}
+	
+	
+	@GetMapping("/test02")
+	public String test02(Model model) {
+		List<String> musicRanking = new ArrayList<>();
+		musicRanking.add("강남스타일");
+		musicRanking.add("벚꽃엔딩");
+		musicRanking.add("좋은날");
+		musicRanking.add("거짓말");
+		musicRanking.add("보고싶다");
+		
+		model.addAttribute("musicRanking", musicRanking);
+		
+		return "jstl/test02";
 	}
 
 }
